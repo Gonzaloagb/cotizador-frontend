4d0aa0f6d0bd0706344cdb8d712aa84bfc9c9e6b
GonzaloForest <gblondi@forestcar.com.ar>
2025-05-20 19:23:09 -0300

    caching vercel

--- pages/linea-hotelera.js
+++ pages/linea-hotelera.js
@@ -54,7 +54,7 @@ export default function LineaHotelera({ productos }) {
 
 export async function getServerSideProps() {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/productos/index.php`);
+    const res = await fetch("https://edabastecimientosyservicios.com.ar/PERSONAL_CotizadorOnlineMySQL/servicio/productos/index.php");
     const productos = await res.json();
     return { props: { productos } };
   } catch (error) {
@@ -62,3 +62,4 @@ export async function getServerSideProps() {
     return { props: { productos: [] } };
   }
 }
+
